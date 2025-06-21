65bc37f5c35d2264c08ab9ed73af81fc45804229
Lucas Gomes <lucaass1997rp@gmail.com>
2025-06-20 21:06:30 -0300

    feat: adicionando logica de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,54 @@ import Link from 'next/link'
 import styles from'./page.module.scss'
 import logoImg from '/public/logo.svg'
 import Image from 'next/image'
+import { api } from '@/services/api'
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
 
 export default function Page() {
+
+    async function  handleLogin(formData: FormData) {
+        "use server"
+
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        if(email === "" || password === "") {
+            return;
+        }
+
+        try {
+            const response = await api.post("session", {
+                email,
+                password
+            })
+
+            if(!response.data.token) {
+                return;
+            }
+
+            console.log(response.data)
+
+            const expressTime = 60 * 60 * 24 * 30 * 1000;
+            const cookieStore = await cookies();
+
+            cookieStore.set("session", response.data.token, {
+                maxAge: expressTime,
+                path: "/",
+                httpOnly: false,
+                secure: process.env.NODE_ENV === "production"
+            })
+
+        } catch(err) {
+            console.log(err)
+            console.log("Erro ao se cadastrar")
+            return
+        }
+
+        
+
+        redirect("/dashboard")
+    }
     return (
         <>
             <div className={styles.containerCenter}>
@@ -13,7 +59,7 @@ export default function Page() {
                 />
 
                 <section className={styles.login}>
-                    <form>
+                    <form action={handleLogin}>
                         <input 
                             type="email" 
                             required
