ce3c5ef28c709bdf17a4aa510bd003f64463d635
Lucas Gomes <lucaass1997rp@gmail.com>
2025-07-08 08:56:00 -0300

    feat: adiciona logica para adiciona imagem

--- src/app/dashboard/product/components/form/index.tsx
+++ src/app/dashboard/product/components/form/index.tsx
@@ -5,10 +5,60 @@ import styles from "./styles.module.scss"
 import { UploadCloud } from "lucide-react"
 import Image from "next/image"
 import { Button } from "@/app/dashboard/components/button"
+import { api } from "@/services/api"
+import { getCookieClient } from "@/lib/cookieClient"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
 
-export function Form() {
+interface CategoryProps {
+    id: string;
+    name: string;
+}
+
+interface Props {
+    categories: CategoryProps[]
+}
+
+export function Form({ categories }: Props) {
+    const router = useRouter();
     const [image, setImage] = useState<File>()
     const [previewImage, setPreviewImage] = useState("")
+
+    async function handleRegisterProduct(formData: FormData) {
+        const categoryIndex = formData.get("category")
+        const name = formData.get("name")
+        const price = formData.get("price")
+        const description = formData.get("description")
+
+        if(!name || !categoryIndex || !price || !description || !image ) {
+            toast.warning("Preencha todos os campos")
+            return
+        }
+
+        const data = new FormData()
+
+        data.append("name", name)
+        data.append("price", price)
+        data.append("description", description)
+        data.append("category_id", categories[Number(categoryIndex)].id)
+        data.append("file", image)
+
+        const token = await getCookieClient()
+ 
+        await api.post("/product", data, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            toast.warning("Falha ao cadastrar esse produto")
+            return
+        })
+
+        toast.success("Produto registrado com sucesso!")
+        router.push("/dashboard")
+    }
 
 
     function handleFile(e: ChangeEvent<HTMLInputElement>) {
@@ -17,7 +67,7 @@ export function Form() {
 
             if(image.type !== "image/jpeg" && image.type !=="image/png" ) {
 
-                console.log("Formato proibido!!")
+                toast.warning("Formato não permitido")
                 return;
             }
 
@@ -31,7 +81,7 @@ export function Form() {
         <main className={styles.container}>
             <h1>Novo Produto</h1>
 
-            <form className={styles.form}>
+            <form className={styles.form} action={handleRegisterProduct}>
 
                 <label className={styles.labelImagem}>
                     <span>
@@ -58,15 +108,11 @@ export function Form() {
                 </label>
 
                 <select name="category">
-                    <option key={1} value="1">
-                       Pizzas
-                    </option>
-                    <option key={1} value="1">
-                       Massas
-                    </option> 
-                    <option key={1} value="1">
-                       Sabores
-                    </option> 
+                    {categories.map( ( category, index ) => (
+                        <option key={category.id} value={index}>
+                            {category.name}
+                        </option>
+                    ))}
                 </select>
 
                 <input 
