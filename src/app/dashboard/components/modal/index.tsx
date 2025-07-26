10d48c33d047df6e9bfd150fed8c6377162a4aac
Lucas Gomes <lucaass1997rp@gmail.com>
2025-07-26 19:58:22 -0300

    feat: adiciona novas funcionalidades

---
+++ src/app/dashboard/components/modal/index.tsx
@@ -0,0 +1,46 @@
+"use client"
+import styles from "./styles.module.scss"
+import { X } from "lucide-react"
+import { OrderContext } from "@/providers/order"
+import { use } from "react"
+
+export function ModalOrder(){
+    const { onRequestClose, order } = use(OrderContext)
+
+    return (
+        <dialog className={styles.dialogContainer}>
+            <section className={styles.dialogContent}>
+                <button className={styles.dialogBlack} onClick={onRequestClose}>
+                    <X size={40} color="#FF3f4b" />
+                </button>
+
+                <article className={styles.container}>
+                    <h2>Detalhes do pedido</h2>
+
+                    <span className={styles.table}>
+                        Mesa <b>{order[0].order.table}</b>
+                    </span>
+                    
+                    {order[0].order?.name && (
+                        <span className={styles.name}>
+                            <b>{order[0].order.name}</b>
+                        </span>
+                    )}
+
+                    {order.map(item => (
+                        <section className={styles.item}>
+                            <span>{item.amount} - <b>{item.product.name}</b></span>
+                            <span className={styles.description}>
+                                {item.product.description}
+                            </span>
+                        </section>
+                    ))}
+
+                    <button className={styles.buttonOrder}>
+                        Concluir pedido
+                    </button>
+                </article>
+            </section>
+        </dialog>
+    )
+}
--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -1,6 +1,11 @@
-import { RefreshCcw } from "lucide-react"
+"use client"
+
 import styles from "./styles.module.scss"
+import { RefreshCcw } from "lucide-react"
 import { OrderProps } from "@/lib/order.type"
+import { ModalOrder } from "../modal"
+import { OrderContext } from "@/providers/order"
+import { use } from "react"
 
 interface Props {
     orders: OrderProps[]
@@ -8,30 +13,37 @@ interface Props {
 
 
 export function Orders({ orders }: Props) {
+    const { isOpen, onRequestOpen} = use(OrderContext)
+
+    async function handleDetailOrder(order_id: string) {
+        await onRequestOpen(order_id)
+    }
     return (
-        <main className={styles.container}>
-            
-            <section className={styles.containerHeader}>
-                <h1>Últimos pedidos</h1>
-                <button>
-                    <RefreshCcw size={24} color="#3fffa3" />
-                </button>
-            </section>
-
-            <section className={styles.listOrders}>
-                {orders.map( order => (
-                    <button
-                    key={order.id}
-                    className={styles.orderItem}
-                    >
-                        <div className={styles.tag}></div>
-                        <span>Mesa {order.table}</span>
+        <>
+            <main className={styles.container}>
+                <section className={styles.containerHeader}>
+                    <h1>Últimos pedidos</h1>
+                    <button>
+                        <RefreshCcw size={24} color="#3fffa3" />
                     </button>
-                ))}
+                </section>
 
-               
+                <section className={styles.listOrders}>
+                    {orders.map( order => (
+                        <button
+                        key={order.id}
+                        className={styles.orderItem}
+                        onClick={ () => handleDetailOrder(order.id)}
+                        >
+                            <div className={styles.tag}></div>
+                            <span>Mesa {order.table}</span>
+                        </button>
+                    ))}
+                </section>
+            </main>
 
-            </section>
-        </main>
+            { isOpen && <ModalOrder /> }
+        </>
+        
     )
 }
