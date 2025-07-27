103fb5a9d61e23e0907b942f82b5e263745b4c33
Lucas Gomes <lucaass1997rp@gmail.com>
2025-07-26 22:26:09 -0300

    feat: adiciona refresh ao modal

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -5,7 +5,9 @@ import { RefreshCcw } from "lucide-react"
 import { OrderProps } from "@/lib/order.type"
 import { ModalOrder } from "../modal"
 import { OrderContext } from "@/providers/order"
-import { use } from "react"
+import { use, useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
 
 interface Props {
     orders: OrderProps[]
@@ -14,21 +16,44 @@ interface Props {
 
 export function Orders({ orders }: Props) {
     const { isOpen, onRequestOpen} = use(OrderContext)
+    const route = useRouter()
 
     async function handleDetailOrder(order_id: string) {
         await onRequestOpen(order_id)
     }
+
+    function handleRefresh() {
+        route.refresh()
+        toast.success("Pedidos atualizado com sucesso!")
+    }
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            route.refresh()
+            toast.success("Pedidos atualizados automaticamente!")
+        }, 600000) // 15 minutos
+
+        // Limpa o intervalo quando o componente desmontar
+        return () => clearInterval(interval)
+    }, [route])
+
     return (
         <>
             <main className={styles.container}>
                 <section className={styles.containerHeader}>
                     <h1>Últimos pedidos</h1>
-                    <button>
+                    <button onClick={handleRefresh}>
                         <RefreshCcw size={24} color="#3fffa3" />
                     </button>
                 </section>
 
                 <section className={styles.listOrders}>
+                    {orders.length === 0 && ( 
+                        <span className={styles.emptyItem}>
+                            Nenhum pedido aberto no momento...
+                        </span>
+                    )}
                     {orders.map( order => (
                         <button
                         key={order.id}
